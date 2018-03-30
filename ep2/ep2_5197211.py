6a8264889edacb851791d54b3c214f06cd1b5f22
Daniel  Caires <danielcaires@Daniels-MacBook-Pro.local>
2018-03-30 14:27:18 -0300

    finalizada a primeira iteração do experimento

--- ep2/ep2_5197211.py
+++ ep2/ep2_5197211.py
@@ -10,18 +10,26 @@ Período: 1o semestre de 2018
 """
 import numpy as np
 
+def retornaLadrilho(coordenadas):
+  saida = [0,0]
+  for i in range(2):
+    saida[i] = int(coordenadas[i]*10)
+  return saida[0],saida[1]
+
+
 potenciasDeN = [5,10,15,20]
-
 matrizLadrilhos = np.zeros((10,10)) #Matriz que contabiliza o numero de pontos por ladrilho
-print matrizLadrilhos
-
-#Gera um par (x,y) de numeros pseudo-aleatorios em [0,1]X[0,1]
-ponto = np.random.rand(2)
-
-#Verifica em qual ladrilho o ponto (x,y) se encontra e contabiliza na matriz de ladrilhos
-
 
 #repete o procedimento n vezes
+for i in range(pow(2,5)):
+  #Gera um par (x,y) de numeros pseudo-aleatorios em [0,1]X[0,1]
+  ponto = np.random.rand(2)
+
+  #Verifica em qual ladrilho o ponto (x,y) se encontra e contabiliza na matriz de ladrilhos
+  ladrilho = retornaLadrilho(ponto)
+  matrizLadrilhos[ladrilho[0]][ladrilho[1]]+=1
+
+print matrizLadrilhos
 #calcula a média, desvio padrão e Coeficiente de variação do número de pontos por ladrilho
 
 #repete o experimento m vezes
---
+++ ep2/python_tester.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+print (pow(2,20))
