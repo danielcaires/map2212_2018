84fae3203dee5a6a92da38ae8c9153a2a625019c
Daniel  Caires <danielcaires@Daniels-MacBook-Pro.local>
2018-03-30 14:04:30 -0300

    criação das variáveis a serem utilizadas

--- ep2/ep2_5197211.py
+++ ep2/ep2_5197211.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 ========================Trabalho 2==========================
 Disciplina: MAP 2212 - Laboratório de Simulação e Computação
@@ -7,12 +8,18 @@ Curso: Bacharelado em Matemática Aplicada e Computacional
 Turma: 54 - Noturno
 Período: 1o semestre de 2018
 """
-import numpy
+import numpy as np
+
+potenciasDeN = [5,10,15,20]
+
+matrizLadrilhos = np.zeros((10,10)) #Matriz que contabiliza o numero de pontos por ladrilho
+print matrizLadrilhos
 
 #Gera um par (x,y) de numeros pseudo-aleatorios em [0,1]X[0,1]
-#Gera um par (x,y) de numeros quasi-aleatorios em [0,1]X[0,1]
+ponto = np.random.rand(2)
 
 #Verifica em qual ladrilho o ponto (x,y) se encontra e contabiliza na matriz de ladrilhos
+
 
 #repete o procedimento n vezes
 #calcula a média, desvio padrão e Coeficiente de variação do número de pontos por ladrilho
