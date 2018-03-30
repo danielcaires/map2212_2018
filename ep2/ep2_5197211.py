e98ce0b3da3f39c728fec9da5c4253a37223defc
Daniel  Caires <danielcaires@Daniels-MacBook-Pro.local>
2018-03-30 12:34:51 -0300

    Criação do esqueleto do EP2

---
+++ ep2/ep2_5197211.py
@@ -0,0 +1,24 @@
+"""
+========================Trabalho 2==========================
+Disciplina: MAP 2212 - Laboratório de Simulação e Computação
+Aluno: Daniel Oliveira Caires
+Número USP: 5197211
+Curso: Bacharelado em Matemática Aplicada e Computacional
+Turma: 54 - Noturno
+Período: 1o semestre de 2018
+"""
+import numpy
+
+#Gera um par (x,y) de numeros pseudo-aleatorios em [0,1]X[0,1]
+#Gera um par (x,y) de numeros quasi-aleatorios em [0,1]X[0,1]
+
+#Verifica em qual ladrilho o ponto (x,y) se encontra e contabiliza na matriz de ladrilhos
+
+#repete o procedimento n vezes
+#calcula a média, desvio padrão e Coeficiente de variação do número de pontos por ladrilho
+
+#repete o experimento m vezes
+#calcula a média dos Coeficientes de Variação e imprime
+
+#Repete o procedimento para o próximo valor de n
+
